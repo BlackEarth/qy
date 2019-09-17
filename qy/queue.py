c7c9e6b81cb2666662adc5ddf0448a84ef42610f
Sean Harrison <sah@bookgenesis.com>
2019-09-16 20:09:30 -0500

    + Add qname to job to be able to filter on different workloads + Revise sqly to match latest

--- qy/queue.py
+++ qy/queue.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 from datamodels import Model
-from sqly import SQL, Dialects
+from sqly import SQL, Query, Dialects
 from sqly.lib import run
 
 
@@ -10,6 +10,7 @@ class Job(Model):
     TABLE = 'qy_jobs'
     PK = ['id']
 
+    qname: str = field()
     id: int = field(default=None)
     retries: int = field(default=3)
     queued: datetime = field(default=None)
@@ -21,24 +22,25 @@ class Job(Model):
 class Queue(Model):
     dialect: Dialects
 
-    def put(self, data, retries=3, scheduled=None):
-        query = SQL(
+    def put(self, qname, data, retries=3, scheduled=None):
+        query = SQL(self.dialect).query(
             """
             INSERT INTO qy_jobs ({fields}) values ({params})
             RETURNING *
             """,
             dialect=self.dialect,
         )
-        job = Job(data=data, retries=retries, scheduled=scheduled)
+        job = Job(qname=qname, data=data, retries=retries, scheduled=scheduled)
         return query.render(job.dict(nulls=False))
 
-    def get(self):
-        query = SQL(
+    def get(self, qname):
+        query = SQL(self.dialect).query(
             """
             UPDATE qy_jobs q1 SET retries = retries - 1
             WHERE q1.id = ( 
                 SELECT q2.id FROM qy_jobs q2 
-                WHERE q2.retries > 0
+                WHERE q2.qname={qname} 
+                AND q2.retries > 0
                 AND q2.scheduled <= now()
                 ORDER BY q2.created FOR UPDATE SKIP LOCKED LIMIT 1 
             )
@@ -46,10 +48,10 @@ class Queue(Model):
             """.rstrip(),
             dialect=self.dialect,
         )
-        return str(query)
+        return query.render(qname=qname)[0]
 
     def delete(self, job):
-        query = SQL(
+        query = SQL(self.dialect).query(
             """
             DELETE FROM qy_jobs WHERE id=:id
             """.rstrip(),
