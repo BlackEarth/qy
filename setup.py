2d49e0065e5e091391eb573910c0e2fc87ca2592
Sean Harrison <sah@bookgenesis.com>
2019-12-26 23:08:49 -0600

    0.2.5 – dynamic install_requires

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, re
 from setuptools import setup, find_packages
 from codecs import open
 
@@ -6,13 +6,25 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(PATH, "setup.json"), encoding='utf-8') as f:
     CONFIG = json.load(f)
-    
+
 with open(os.path.join(PATH, 'README.md'), encoding='utf-8') as f:
     README = f.read()
 
-if __name__=='__main__':
+
+def req_from_file(filename):
+    """
+    Return a list of requirements, suitable for setup.py install, from a pip -r req.txt file
+    """
+    txt = open(filename, 'rb').read().decode('utf-8')
+    lines = re.split(r'[\n\r]+', txt)
+    return [line.split('#')[0].strip() for line in lines if line and line[0:2] != '-r']
+
+
+if __name__ == '__main__':
     setup(
         long_description=README,
         packages=find_packages(),
+        include_package_data=True,
+        install_requires=req_from_file(os.path.join(PATH, 'req/install.txt')),
         **CONFIG
     )
