6d6ec24038c4e2f5f3a2d3348a42aefb82f53dfc
Sean Harrison <sah@kruxia.com>
2021-11-06 14:31:20 -0500

    + switch from datamodels to pydantic + use sqly Dialect.render instead of sqly Query

--- qy/queue.py
+++ qy/queue.py
@@ -1,53 +1,41 @@
-import json
 import logging
-from dataclasses import dataclass, field
 from datetime import datetime
-from datamodels import Model
-from sqly import SQL, Query, Dialects
-from sqly.lib import run
+
+from pydantic import BaseModel, Field
+from sqly.dialect import Dialect
 
 logger = logging.getLogger(__name__)
 
-@dataclass
-class Job(Model):
-    TABLE = 'qy_jobs'
-    PK = ['id']
 
-    qname: str = field()
-    id: int = field(default=None)
-    retries: int = field(default=3)
-    queued: datetime = field(default=None)
-    scheduled: datetime = field(default=None)
-    data: dict = field(default_factory=dict)
-
-    class CONVERTERS:
-        def data(value):
-            if isinstance(value, str):
-                return json.loads(value)
-            else: 
-                return value
+class Job(BaseModel):
+    qname: str = Field()
+    id: int = Field(default=None)
+    retries: int = Field(default=3)
+    queued: datetime = Field(default=None)
+    scheduled: datetime = Field(default=None)
+    data: dict = Field(default_factory=dict)
 
 
-@dataclass
-class Queue(Model):
+class Queue(BaseModel):
     qname: str
-    dialect: Dialects
+    dialect: Dialect
 
     def __post_init__(self):
         logger.info('Queue initialized: %r' % self)
 
     def put(self, data, retries=3, scheduled=None):
-        query = SQL(self.dialect).query(
-            """
-            INSERT INTO qy_jobs ({fields}) values ({params})
-            RETURNING *
-            """
-        )
         job = Job(qname=self.qname, data=data, retries=retries, scheduled=scheduled)
-        return query.render(job.dict(nulls=False))
+        return self.dialect.render(
+            """
+            INSERT INTO qy_jobs (qname, retries, scheduled, data) 
+            VALUES (:qname, :retries, :scheduled, :data)
+            RETURNING *
+            """,
+            job.dict(),
+        )
 
     def get(self):
-        query = SQL(self.dialect).query(
+        return self.dialect.render(
             """
             UPDATE qy_jobs q1 SET retries = retries - 1
             WHERE q1.id = ( 
@@ -59,15 +47,14 @@ class Queue(Model):
                 FOR UPDATE SKIP LOCKED LIMIT 1 
             )
             RETURNING q1.*;
-            """.rstrip()
+            """,
+            {'qname': self.qname}
         )
-        return query.render({'qname': self.qname})
 
     def delete(self, job):
-        query = SQL(self.dialect).query(
+        return self.dialect.render(
             """
             DELETE FROM qy_jobs WHERE id=:id
-            """.rstrip(),
-            dialect=self.dialect,
+            """,
+            {'id': job.id},
         )
-        return query.render({'id': job.id})
